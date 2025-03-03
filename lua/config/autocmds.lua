7545bd902f149a30e2f523062ed5bae8f67a4fb2
Aniket Limaye <a-limaye@ti.com>
2025-03-03 13:47:00 +0530

    config: autocmds.lua: RST file settings
    Signed-off-by: Aniket Limaye <a-limaye@ti.com>

--- lua/config/autocmds.lua
+++ lua/config/autocmds.lua
@@ -6,3 +6,14 @@
 --
 -- Or remove existing autocmds by their group name (which is prefixed with `lazyvim_` for the defaults)
 -- e.g. vim.api.nvim_del_augroup_by_name("lazyvim_wrap_spell")
+
+-- Set correct indentation for RST files
+vim.api.nvim_create_autocmd("FileType", {
+  group = vim.api.nvim_create_augroup("Rst_file_defaults", { clear = true }),
+  pattern = { "rst" },
+  callback = function()
+    vim.opt_local.tabstop = 3
+    vim.opt_local.shiftwidth = 3
+    vim.opt_local.expandtab = true
+  end,
+})
