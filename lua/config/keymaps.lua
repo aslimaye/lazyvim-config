92dee4dcc476d1090f3bd8b7b1db68a74a9eb4a7
Aniket Limaye <a-limaye@ti.com>
2025-03-03 13:47:53 +0530

    config: keymap.lua: My Keymaps
    Signed-off-by: Aniket Limaye <a-limaye@ti.com>

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -1,3 +1,14 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+
+-- Redo
+vim.keymap.set("n", "U", "<C-r>", { desc = "Redo", silent = false })
+
+-- Move to start/end of line
+vim.keymap.set({ "n", "v" }, "H", "<Home>", { desc = "StartOfLine", silent = true })
+vim.keymap.set({ "n", "v" }, "L", "<End>", { desc = "EndOfLine", silent = true })
+
+-- buffers
+vim.keymap.set("n", "<A-h>", "<cmd>bprevious<cr>", { desc = "Prev Buffer" })
+vim.keymap.set("n", "<A-l>", "<cmd>bnext<cr>", { desc = "Next Buffer" })
